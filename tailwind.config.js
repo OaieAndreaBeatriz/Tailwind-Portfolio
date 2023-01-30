bc7be34049a72ed7098891b04357020a2c0c2c3c
Oaie Andrea-Beatriz <oaie.andrea.beatriz@gmail.com>
2023-01-30 12:45:08 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './src/components/About.js',
+    './src/components/Contact.js',
+    './src/components/Home.js',
+    './src/components/NavBar.js',
+    './src/components/Projects.js',
+    './src/components/Skills.js',
+    './src/App.js'
+  ],
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
