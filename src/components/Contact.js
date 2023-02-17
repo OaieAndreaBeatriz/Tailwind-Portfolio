bbc114a880ea4bdd5d5bd6f8fc97d0f7905a1c42
Oaie Andrea-Beatriz <oaie.andrea.beatriz@gmail.com>
2023-02-17 18:16:08 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ function Contact() {
             Contact
           </p>
           <p className='text-gray-300 py-4'>
-            Submit the form below or shoot me an email - oaie.andrea@gmail.com
+            Submit the form below or shoot me an email - oaie.andrea.beatriz@gmail.com
           </p>
         </div>
         <input
