97b5e14c3f1dcc58d761b4b31dfc097d45e76a2f
Oaie Andrea-Beatriz <oaie.andrea.beatriz@gmail.com>
2023-04-10 15:38:56 +0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
-import CV from '../assets/CV-Oaie Andrea Beatriz.pdf';
+import CV from '../assets/CV - Oaie Andrea Beatriz.pdf';
 
 function NavBar() {
   const [nav, setNav] = useState(false);
