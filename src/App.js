1246133c3041847dc699acbbac957d0263813f91
Oaie Andrea-Beatriz <oaie.andrea.beatriz@gmail.com>
2023-04-19 21:42:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import About from './components/About';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
-  );
-}
-
-export default App;
+qergq3g
+24g24f
+w45g
+w
+rg
+245g
+23
+5g2
+45g
